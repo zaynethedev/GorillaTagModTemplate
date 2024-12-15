c9347b44d3cc1cb80bac1fbbbf25e9e7aaa298e4
Zayne <142189531+zaynethedev@users.noreply.github.com>
2024-12-14 22:53:50 -0800

    Add files via upload

---
+++ src/Content/GorillaTagModTemplate/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using UnityEngine;
+
+namespace GorillaTagModTemplateProject
+{
+	[BepInPlugin("AuthorLowerName.GorillaTagLowerModTemplateProject", "GorillaTagModTemplateProject", "1.0.0")]
+	public class Plugin : BaseUnityPlugin
+	{
+		bool inRoom;
+        public const string InstanceId = PluginInfo.GUID;
+
+        void Start()
+		{
+            var harmony = Harmony.CreateAndPatchAll(GetType().Assembly, "AuthorLowerName.GorillaTagLowerModTemplateProject");
+            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+		}
+
+		void OnGameInitialized()
+		{
+
+		}
+
+		void Update()
+		{
+			if (NetworkSystem.Instance.InRoom && NetworkSystem.Instance.GameModeString.Contains("MODDED"))
+			{
+				if (!inRoom)
+				{
+					inRoom = true;
+				}
+			}
+			else
+			{
+                if (inRoom)
+                {
+                    inRoom = false;
+                }
+            }
+		}
+	}
+}
